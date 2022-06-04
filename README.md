99acb9b36f966d57a1c04a9ecbb3e364e5fc7ca5
陈鹏达 <chenpengda@oschina.cn>
2022-06-04 22:40:00 +0800

    feat: init site

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# Exercise Next.js
+
+我想要练习 next.js 根据 [next.js](https://nextjs.org)
+
+模仿 [起点](https://www.qidian.com/) 网站
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
@@ -59,7 +59,8 @@ export default function Home() {
         </a>
       </footer>
 
-      <style jsx>{`
+      <style jsx>
+        {`
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
